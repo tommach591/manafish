b338d7dfcde6437e35087655ce0239dd42936dff
Tom Mach <57111473+tommach591@users.noreply.github.com>
2024-10-21 21:13:42 -0700

    Update SeedMenu.jsx

--- src/components/Garden/SeedMenu/SeedMenu.jsx
+++ src/components/Garden/SeedMenu/SeedMenu.jsx
@@ -28,7 +28,7 @@ function SeedMenu({ tool, setTool }) {
 
   return (
     <div className="SeedMenu">
-      <h1 className="CurrentSeason">Current Season: {getSeason(season)}</h1>
+      <h1 className="CurrentSeason">{getSeason(season)}</h1>
       {Object.entries(gardendex)
         .filter(([key]) => gardenShop.includes(key))
         .sort((a, b) => {
@@ -44,13 +44,24 @@ function SeedMenu({ tool, setTool }) {
               onClick={() => {
                 setTool(Number(value.id));
               }}
-              style={
-                tool === Number(value.id) ? { opacity: 0.3 } : { opacity: 1 }
-              }
+              style={{
+                border:
+                  value.season === season
+                    ? "1px solid green"
+                    : "1px solid black",
+                background:
+                  value.season === season
+                    ? "rgb(200, 255, 225)"
+                    : "rgb(255, 255, 255)",
+                opacity: tool === Number(value.id) ? 0.3 : 1,
+              }}
             >
               <img src={getPlantImage(key)} alt="" />
               <div className="SeedInfo">
-                <h1>{value.name}</h1>
+                <h1>
+                  {value.name}
+                  {season === value.season ? " (x2)" : ""}
+                </h1>
                 <h3>Cost: {Math.floor(value.value / PRICE)} Mana</h3>
                 <h3>Growth: {convertToHoursAndMinutes(value.growth)}</h3>
                 <h3>Season: {getSeason(value.season)}</h3>
